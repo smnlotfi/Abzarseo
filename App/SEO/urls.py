6dac00290e141215678571489c5ba6446d00a52a
saman <smnlotfi@gmail.com>
2023-08-06 21:54:55 +0330

    Fixed settings.py

--- App/SEO/urls.py
+++ App/SEO/urls.py
@@ -14,14 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from django.shortcuts import redirect
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('accounting.urls')),
-    path('',include('panel.urls')),
-    path('accounts/', include('allauth.urls')),
-    path('accounts/profile/', lambda request: redirect('/', permanent=False)),
+    path("admin/", admin.site.urls),
+    path("", include("accounting.urls")),
+    path("", include("panel.urls")),
+    path("accounts/", include("allauth.urls")),
+    path("accounts/profile/", lambda request: redirect("/", permanent=False)),
 ]
